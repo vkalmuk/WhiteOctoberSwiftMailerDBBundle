c65053ce33ba62b855623060b6bd8a157de51abd
Victor Kalmuk <victor.kalmuk@gmail.com>
2021-03-31 16:30:25 +0300

    Add Doctrine 2.7 support.

--- Spool/DatabaseSpool.php
+++ Spool/DatabaseSpool.php
@@ -3,9 +3,8 @@
 namespace WhiteOctober\SwiftMailerDBBundle\Spool;
 
 use AbstractEmail;
-use Doctrine\Common\Persistence\ManagerRegistry;
-use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use WhiteOctober\SwiftMailerDBBundle\EmailInterface;
 
 /**
@@ -35,19 +34,13 @@ class DatabaseSpool extends \Swift_ConfigurableSpool
     protected $environment;
 
     /**
-     * @var ObjectManager
+     * @var EntityManagerInterface
      */
     protected $em;
 
-    /**
-     * @param RegistryInterface $doc
-     * @param AbstractEmail $entityClass
-     * @param string $environment
-     * @param bool $keepSentMessages
-     */
-    public function __construct(RegistryInterface $doc, $entityClass, $environment, $keepSentMessages = false)
+    public function __construct(ManagerRegistry $doc, $entityClass, $environment, $keepSentMessages = false)
     {
-        $this->doc              = $doc;
+        $this->doc = $doc;
         $this->keepSentMessages = $keepSentMessages;
 
         /** @var AbstractEmail $obj */
@@ -107,7 +100,7 @@ class DatabaseSpool extends \Swift_ConfigurableSpool
     /**
      * Sends messages using the given transport instance.
      *
-     * @param \Swift_Transport $transport         A transport instance
+     * @param \Swift_Transport $transport A transport instance
      * @param string[]        &$failedRecipients An array of failures by-reference
      *
      * @return int The number of sent emails
@@ -129,7 +122,7 @@ class DatabaseSpool extends \Swift_ConfigurableSpool
             return 0;
         }
 
-        $failedRecipients = (array) $failedRecipients;
+        $failedRecipients = (array)$failedRecipients;
         $count = 0;
         $time = time();
         foreach ($emails as $email) {
@@ -137,7 +130,7 @@ class DatabaseSpool extends \Swift_ConfigurableSpool
             $em->persist($email);
             $em->flush();
 
-            $message = \unserialize($email->getMessage(),null);
+            $message = \unserialize($email->getMessage(), null);
 
             if (!$transport->isStarted()) {
                 $transport->start();
@@ -160,10 +153,167 @@ class DatabaseSpool extends \Swift_ConfigurableSpool
         return $count;
     }
 
+    protected function getManager(): EntityManagerInterface
+    {
+        return $this->doc->getManagerForClass($this->entityClass);
+    }
+}
+
+
+namespace WhiteOctober\SwiftMailerDBBundle\Spool;
+
+use AbstractEmail;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use WhiteOctober\SwiftMailerDBBundle\EmailInterface;
+
+/**
+ * Class DatabaseSpool
+ * @package WhiteOctober\SwiftMailerDBBundle\Spool
+ */
+class DatabaseSpool extends \Swift_ConfigurableSpool
+{
     /**
-     * @return ObjectManager
+     * @var ManagerRegistry
      */
-    protected function getManager(): ObjectManager
+    protected $doc;
+
+    /**
+     * @var AbstractEmail
+     */
+    protected $entityClass;
+
+    /**
+     * @var boolean
+     */
+    protected $keepSentMessages;
+
+    /**
+     * @var string
+     */
+    protected $environment;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
+
+    public function __construct(ManagerRegistry $doc, $entityClass, $environment, $keepSentMessages = false)
+    {
+        $this->doc = $doc;
+        $this->keepSentMessages = $keepSentMessages;
+
+        /** @var AbstractEmail $obj */
+        $obj = new $entityClass;
+        if (!$obj instanceof EmailInterface) {
+            throw new \InvalidArgumentException("The entity class '{$entityClass}'' does not extend from EmailInterface");
+        }
+
+        $this->entityClass = $entityClass;
+        $this->environment = $environment;
+        $this->em = $this->getManager();
+    }
+
+    /**
+     * Starts this Spool mechanism.
+     */
+    public function start()
+    {
+    }
+
+    /**
+     * Stops this Spool mechanism.
+     */
+    public function stop()
+    {
+    }
+
+    /**
+     * Tests if this Spool mechanism has started.
+     *
+     * @return boolean
+     */
+    public function isStarted()
+    {
+        return true;
+    }
+
+    /**
+     * Queues a message.
+     *
+     * @param \Swift_Mime_SimpleMessage $message The message to store
+     * @return boolean Whether the operation has succeeded
+     */
+    public function queueMessage(\Swift_Mime_SimpleMessage $message)
+    {
+        /** @var AbstractEmail $mailObject */
+        $mailObject = new $this->entityClass;
+        $mailObject->setMessage(serialize($message));
+        $mailObject->setStatus(EmailInterface::STATUS_READY);
+        $mailObject->setEnvironment($this->environment);
+        $this->em->persist($mailObject);
+        $this->em->flush();
+
+        return true;
+    }
+
+    /**
+     * Sends messages using the given transport instance.
+     *
+     * @param \Swift_Transport $transport A transport instance
+     * @param string[]        &$failedRecipients An array of failures by-reference
+     *
+     * @return int The number of sent emails
+     */
+    public function flushQueue(\Swift_Transport $transport, &$failedRecipients = null)
+    {
+        $em = $this->em;
+        $repoClass = $this->em->getRepository($this->entityClass);
+        $limit = $this->getMessageLimit();
+        $limit = $limit > 0 ? $limit : null;
+
+        /** @var AbstractEmail[] $emails */
+        $emails = $repoClass->findBy(
+            array('status' => EmailInterface::STATUS_READY, 'environment' => $this->environment),
+            null,
+            $limit
+        );
+        if (!\count($emails)) {
+            return 0;
+        }
+
+        $failedRecipients = (array)$failedRecipients;
+        $count = 0;
+        $time = time();
+        foreach ($emails as $email) {
+            $email->setStatus(EmailInterface::STATUS_PROCESSING);
+            $em->persist($email);
+            $em->flush();
+
+            $message = \unserialize($email->getMessage(), null);
+
+            if (!$transport->isStarted()) {
+                $transport->start();
+            }
+
+            $count += $transport->send($message, $failedRecipients);
+            if ($this->keepSentMessages === true) {
+                $email->setStatus(EmailInterface::STATUS_COMPLETE);
+                $em->persist($email);
+            } else {
+                $em->remove($email);
+            }
+            $em->flush();
+
+            if ($this->getTimeLimit() && (time() - $time) >= $this->getTimeLimit()) {
+                break;
+            }
+        }
+
+        return $count;
+    }
+
+    protected function getManager(): EntityManagerInterface
     {
         return $this->doc->getManagerForClass($this->entityClass);
     }
